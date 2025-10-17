ef235a131e6090ecb4872a585235550b701904c4
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2025-10-17 15:26:12 +0100

    add raise errors

--- src/snomedct_icd10_mapping.py
+++ src/snomedct_icd10_mapping.py
@@ -13,36 +13,69 @@ import collections
 PathLike = Union[Path | str]
 
 
-def init_db(path_umls_data: PathLike, path_sqldb: PathLike, terminologies: List[str] = None):
+def init_db(dir_umls_data: PathLike, dir_sqldb: PathLike, terminologies: List[str] = None):
     """
     Build a database from UMLS data.
     please download UMLS data here: https://www.nlm.nih.gov/research/umls/licensedcontent/umlsknowledgesources.html
     only need to run this function once.
 
-    :param path_umls_data: Absolute path (do not use ~/) to the UMLS data (zip format). e.g.,/PATH/YOUR/DIRECTORY/umls-2025AA-metathesaurus-full.zip
-    :param path_sqldb: Absolute path for ontology database with database name e.g.,/PATH/YOUR/DIRECTORY/db.sqlite3
+    :param dir_umls_data: Absolute path (do not use ~/) to the UMLS data (zip format). e.g.,/PATH/YOUR/DIRECTORY/umls-2025AA-metathesaurus-full.zip
+    :param dir_sqldb: Absolute path for ontology database with database name e.g.,/PATH/YOUR/DIRECTORY
     :param terminologies: by default, terminologies includes ICD10, SNOMEDCT_US, and CUI
     """
+    try:
+        # Check if directory exists before using it
+        if not dir_sqldb.exists():
+            raise FileNotFoundError(f"Directory not found: {dir_sqldb}")
 
-    default_world.set_backend(filename=path_sqldb,
-                              exclusive=False)  # data are stored in path_sqldb which is the quadstore file.
+        default_world.set_backend(filename=dir_sqldb / 'med_terminology.sqlite3',
+                                  exclusive=False)  # data are stored in path_sqldb which is the quadstore file.
+        print(f'SQL database has been created at {dir_sqldb}')
+
+    except (ValueError, FileNotFoundError) as e:
+        print(f"Error: {e}")
+        print("Please check your directory path or database file.")
+
     print('Setting backend...')
     if terminologies is None:
         # CUI correspond to concepts: a given concept gathers equivalent terms or codes from various terminologies.
         terminologies = ["ICD10", "SNOMEDCT_US", "CUI"]
-    print(f'SQL database has been created at {path_sqldb}')
-    import_umls(path_umls_data, terminologies)
-    default_world.save()  # for using persistence
-    print(f'Medical ontology from UMLS data is successfully saved at {path_sqldb} as a SQLite Database')
+
+    try:
+        # check if directory exists before using it
+        if not dir_umls_data.exists():
+            raise FileNotFoundError(f"Directory not found:{dir_umls_data}")
+        umls_zip = dir_umls_data / 'umls-2025AA-metathesaurus-full.zip'
+        if not umls_zip.exists():
+            print(
+                f"Please see contents on this directory {[x.name for x in dir_umls_data.iterdir() if x.suffix == '.zip']}")
+
+            raise FileNotFoundError(f"UMLS data file not found: {umls_zip}")
+
+        import_umls(dir_umls_data / 'umls-2025AA-metathesaurus-full.zip', terminologies)
+        default_world.save()  # for using persistence
+        print(f'Medical ontology from UMLS data is successfully saved at {dir_sqldb} as a SQLite Database')
+    except (ValueError, FileNotFoundError) as e:
+        print(f"Error:{e}")
+        print("Please check your directory path or UMLS data file.")
 
 
-def open_db(path_sqldb: PathLike):
+def open_db(dir_sqldb: PathLike):
     """
     Loading the medical ontology from path_sqldb (e.g., db.sqlite3) built from init_db
-    :param path_sqldb: path of db.sqlite3
+    :param dir_sqldb: Directory of db.sqlite3
     """
     owlready2 = World()  # build a clean world
-    owlready2.set_backend(filename=path_sqldb)  # load .sqlite3 database
+
+    try:
+        # Check if directory exists before using it
+        if not dir_sqldb.exists():
+            raise FileNotFoundError(f"Directory not found: {dir_sqldb}")
+
+        owlready2.set_backend(filename=dir_sqldb / 'med_terminology.sqlite3')  # load .sqlite3 database
+    except (ValueError, FileNotFoundError) as e:
+        print(f"Error: {e}")
+        print("Please check your directory path or database file.")
     med_ontology = owlready2.get_ontology("http://PYM/").load()
     return owlready2, med_ontology
 
@@ -139,13 +172,12 @@ def mapping_icd10(keywords: str, refining_term: str = None, map_child: bool = Tr
 
 
 if __name__ == '__main__':
-    umls_path = '/PATH/YOUR/DIRECTORY/umls-2025AA-metathesaurus-full.zip'
-    path_sqldb = '/PATH/YOUR/DIRECTORY/med_terminology.sqlite3'
-    init_db(umls_path, path_sqldb)
-    owl, med_ontology = open_db(path_sqldb)
+    dir_umls = Path('/PATH/YOUR/DIRECTORY/')
+    dir_sqldb = Path('/PATH/YOUR/DIRECTORY/')
+    init_db(dir_umls, dir_sqldb)
+    owl, med_ontology = open_db(dir_sqldb)
     snomedct = med_ontology["SNOMEDCT_US"]
     icd10 = med_ontology["ICD10"]
     keywords = 'breast cancer'
-    all_hierarchy = get_hierarchy(keywords=keywords, ancestor=False, descendant=True,
-                                  output_path='snomed.csv')
+    all_hierarchy = get_hierarchy(keywords=keywords, ancestor=False, descendant=True, output_path='snomed.csv')
     mapping_icd10(keywords=keywords, refining_term='hormone receptor positive', output_path='snomed_icd_mapping.csv')
